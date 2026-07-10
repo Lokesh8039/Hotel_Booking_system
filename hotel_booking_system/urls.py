64f585ddbc0ef49ec3b48643f7db592d31ede606
Lokesh D S <dslokesh8039@gmail.com>
2026-07-10 22:53:01 +0530

    Enable media files serving in production using Django static serve view

--- hotel_booking_system/urls.py
+++ hotel_booking_system/urls.py
@@ -1,15 +1,16 @@
-
 from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
+
 urlpatterns = [
     path('', include("home.urls")),
     path('account/', include("accounts.urls")),
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
