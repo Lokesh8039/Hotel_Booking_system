bafb7940a2dd1901f26a4035b0234ebf8b8283e4
Lokesh D S <dslokesh8039@gmail.com>
2026-07-11 08:57:12 +0530

    Add ping keep-alive endpoint for UptimeRobot pinger integration

--- hotel_booking_system/urls.py
+++ hotel_booking_system/urls.py
@@ -4,8 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.urls import re_path
+from django.http import JsonResponse
+
+def ping(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path('ping/', ping, name='ping'),
     path('', include("home.urls")),
     path('account/', include("accounts.urls")),
     path('admin/', admin.site.urls),
